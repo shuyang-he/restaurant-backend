c2cc5f47f18e7dd2c4e548303710b8a759d0f790
shuyang-he <she26@wustl.edu>
2019-10-02 12:04:29 -0700

    basic structure

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const usersRoute = require('./routes/usersRoute');
+
+const port = 8080;
+const app = express();
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.post('/signup', usersRoute);
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose
+  .connect(
+    'mongodb+srv://amy:amy@cluster0-ee6d2.mongodb.net/human_resource_management_system?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    app.listen(port);
+  })
+  .catch(err => console.log(err));
---
+++ models/restaurant.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Restaurant', restaurantSchema);
